5d2f8fbc7bf6f84f690cc7229babb06b860736d4
Xi Ouyang <simonouyang@gmail.com>
2024-11-13 13:35:03 -0800

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EcommerceModule } from './ecommerce/ecommerce.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), EcommerceModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), EcommerceModule, HealthModule],
   controllers: [],
   providers: [],
 })
---
+++ src/health/health.controller.ts
@@ -0,0 +1,20 @@
+// health.controller.ts
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+    constructor(
+        private health: HealthCheckService,
+        private http: HttpHealthIndicator,
+    ) {}
+
+    @Get()
+    @HealthCheck()
+    check() {
+        return this.health.check([
+            async () => this.http.pingCheck('google', 'https://www.google.com'),
+        ]);
+    }
+}
+
